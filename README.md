d71c9c5ff9d699ccacada835b880343b830ee8c9
h13i32maru <h13i32maru@gmail.com>
2015-03-15 12:10:32 +0900

    chore: support coverall.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
+[![Build Status](https://travis-ci.org/h13i32maru/ice-cap.svg?branch=master)](https://travis-ci.org/h13i32maru/ice-cap)
 # Ice Cap
--- script/test.sh
+++ script/test.sh
@@ -3,9 +3,14 @@
 ./script/build.sh
 ./script/build_test.sh
 
-if [ "$1" == "coverage" ]
+mocha_option="--require out/test/src/espower-loader.js --require ./node_modules/babel/polyfill.js --recursive out/test/src -R spec"
+
+if [ "$TRAVIS" == "1" ]
 then
-    ./node_modules/.bin/istanbul cover node_modules/mocha/bin/_mocha  -- --require out/test/src/espower-loader.js --require ./node_modules/babel/polyfill.js --recursive out/test/src -R tap
+    ./node_modules/.bin/istanbul cover ./node_modules/mocha/bin/_mocha --report lcovonly -- $mocha_option && cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js
+elif [ "$1" == "coverage" ]
+then
+    ./node_modules/.bin/istanbul cover ./node_modules/mocha/bin/_mocha  -- $mocha_option
 else
-    ./node_modules/.bin/mocha  --require out/test/src/espower-loader.js --require ./node_modules/babel/polyfill.js --recursive out/test/src
+    ./node_modules/.bin/mocha $mocha_option
 fi
